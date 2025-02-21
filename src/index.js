7b1a83588caf84ae48f2c5a9881eac1b81d4e507
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-02-21 13:14:04 +0000

    Finish home.js
    -Implement homepage content in home.js instead of html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'modern-normalize/modern-normalize.css';
 import "./styles.css";
+import {home} from './pages/home.js'
 
+home();
 console.log("This Works!")
---
+++ src/pages/home.js
@@ -0,0 +1,29 @@
+import backgroundImageSrc from '../images/background-image.jpg'
+
+// Home Tab content
+export function home() {
+    const contentContainer = document.getElementById("content");
+
+    const mainSection = document.createElement("div");
+    mainSection.setAttribute("id", "main");
+    
+    const backgroundImage = document.createElement("img");
+    backgroundImage.src = backgroundImageSrc;
+    backgroundImage.alt = "Tea pot background image"
+    backgroundImage.classList.add("fade-in");
+    
+    const title = document.createElement("h1");
+    title.textContent = "Tea Time Bliss";
+    const description = document.createElement("p");
+    description.textContent = "Your daily dose of tranquility";
+    const button = document.createElement("button");
+    button.setAttribute("id", "find-out-btn");
+    button.textContent = "Find out more";
+    
+    contentContainer.appendChild(mainSection);
+    mainSection.appendChild(backgroundImage);
+    mainSection.appendChild(title);
+    mainSection.appendChild(description);
+    mainSection.appendChild(button);
+}
+
